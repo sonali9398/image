58232e13f8e1be54116e36fa6568ebd624316f9f
sonali9398 <sonalikarangutkar9398@gmail.com>
2024-01-24 16:17:12 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Slider from './components/Slider';
+import pics from './constants.json'
 
 function App() {
   return (
     <div className="App">
-      <Slider/>
+        <h1>Image Slider</h1>
+
+      <Slider data={pics}/>
     </div>
   );
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,14 +1,34 @@
-import React from 'react'
-import pics from '../constants'
-const Slider = () => {
-  return (
-    <div>
-        <h1>Image Slider</h1>
+import React, { useState } from 'react'
+import './Slider.css'
+import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from 'react-icons/bs'
+
+const Slider = ({data}) => {
+    const[slide, setSlide] = useState(0);
+    
+    function nextSlide(){
+        setSlide(slide === data.pics.length - 1 ? 0 : slide + 1);
+        console.log(setSlide)
+    }
+
+    function prevSlide(){
+        setSlide(slide === 0 ? data.pics.length - 1 : slide - 1);
+    }
+
+   return (
+    <div className='carousel'>
+
+        <BsArrowLeftCircleFill className='arrow left' onClick={prevSlide}/>
         {
-            pics.map((curr) => {
-                return curr
+            data.pics.map((curr, index) => {
+                return <img src={curr.src} alt={curr.alt} key={index} className={slide === index ? "slide" : 'slide-hidden'}/>
             })
         }
+        <BsArrowRightCircleFill className='arrow right' onClick={nextSlide}/>
+        <span className='indicators'>
+            {data.pics.map((_, index) =>{
+                return <button key={index} onClick={() => setSlide(index)} className={slide === index ? "indicator" : "indicator indicator-inactive"}></button>
+            })}
+        </span>
     </div>
   )
 }
